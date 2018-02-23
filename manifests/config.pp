0cd05d56c4a4183514489b973485f67e96844b24
William Kimball <30981667+wwkimball@users.noreply.github.com>
2018-02-23 14:18:49 -0600

    PDK generated module

---
+++ manifests/config.pp
@@ -0,0 +1,10 @@
+# puppet_agent::config
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include puppet_agent::config
+class puppet_agent::config {
+}
---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# puppet_agent
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include puppet_agent
+class puppet_agent {
+}
